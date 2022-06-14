5371d6d15c2deb94da9e6cc9075adfaaa0f5b625
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-06-14 14:26:34 +0800

    setup project name

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
-(define collection "racket-project")
+(define collection "classical-propositional-logic")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "cover"))
-(define scribblings '(("scribblings/racket-project.scrbl" ())))
+(define scribblings '(("scribblings/classical-propositional-logic.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define license '(Apache-2.0 OR MIT))
---
+++ scribblings/classical-propositional-logic.scrbl
@@ -0,0 +1,11 @@
+#lang scribble/manual
+
+@require[@for-label[classical-propositional-logic
+                    racket/base]]
+
+@title{classical-propositional-logic}
+@author{Lîm Tsú-thuàn}
+
+@defmodule[classical-propositional-logic]
+
+Package Description Here
