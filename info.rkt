267de3ae0996af3cb8cc12ca9a98ba17eb5faeed
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-06-14 14:28:04 +0800

    remove scribblings

--- info.rkt
+++ info.rkt
@@ -2,7 +2,6 @@
 (define collection "classical-propositional-logic")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "cover"))
-(define scribblings '(("scribblings/classical-propositional-logic.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define license '(Apache-2.0 OR MIT))
