243cce6c2550233e4b4a37291104b539b05d72fd
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-06-19 02:15:06 +0800

    start unification

--- main.rkt
+++ main.rkt
@@ -21,7 +21,7 @@
         (for ([c (in-combinations (append (set->list kb) (set->list not-q)) 2)])
           (define resolvents (resolve (first c) (second c)))
           (if (set-empty? resolvents)
-              (return #t)
+              (return (format "~a |- ~a" kb-rules query))
               (begin
                 (set! new
                       (set-add new
@@ -33,6 +33,24 @@
               (loop (set-union kb new)))))))
   resolution)
 
+(define (occurs v t)
+  (match t
+    [`(,t* ...)
+     (ormap (λ (t) (occurs v t)) t*)]
+    [t (equal? v t)]))
+
+(define (unify t1 t2)
+  (match* (t1 t2)
+    [(_ t2) #:when (parameter? t2)
+            (if (or (eqv? t1 (t2)) (not (occurs (t2) t1)))
+                (t2 t1)
+                (error (format "~a occurs in ~a" (t2) t1)))]
+    [(t1 _) #:when (parameter? t1)
+            (unify t2 t1)]
+    [(`(,a* ...) `(,b* ...))
+     (andmap unify a* b*)]
+    [(_ _) (eqv? t1 t2)]))
+
 (module+ main
   (define resolution-K (make-resolution K->cnf eq?))
   (resolution-K '((→ A B)
@@ -42,7 +60,8 @@
   (resolution-K '((¬ A))
                 'A)
 
-  (define resolution-KF (make-resolution KF->cnf eq?))
+  (define resolution-KF (make-resolution KF->cnf unify))
+  ;; FIXME: unification should correct this result
   (resolution-KF '((∀ (a) (→ (Red a)
                              (Sweet a)))
                    (Red Apple))
